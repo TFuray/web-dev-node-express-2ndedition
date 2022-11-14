ceff160014521c1d883ef0b47e8a0d15424c4cf5
TFuray <pk.master881@gmail.com>
2022-11-14 12:56:53 -0800

    fixed views

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -1,24 +1,17 @@
 const express = require('express')
-const { engine } = require('express-handlebars')
-const port = process.env.PORT || 3003
-const fortune = require('./lib/fortune')
-const handlers = require('./lib/handlers')
+const expressHandlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const multiparty = require('multiparty')
+
+const handlers = require('./lib/handlers')
+const weatherMiddlware = require('./lib/middleware/weather')
 
 const app = express()
 
-// configure middleware
-app.use(express.static(__dirname + './public'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-// Handlebars
+// configure Handlebars view engine
 app.engine(
   'handlebars',
-  engine({
-    extname: 'handlebars',
-    // defaultLayout: false,
-    layoutsDir: './views/layouts/',
+  expressHandlebars({
     defaultLayout: 'main',
     helpers: {
       section: function (name, options) {
@@ -30,45 +23,35 @@ app.engine(
   })
 )
 app.set('view engine', 'handlebars')
-app.set('views', './views')
 
-// Handlers
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 3003
+
+app.use(express.static(__dirname + '/public'))
+
+app.use(weatherMiddlware)
+
 app.get('/', handlers.home)
-app.get('/about', handlers.about)
+
+// handlers for browser-based form submission
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
+
+// handlers for fetch/JSON form submission
+app.get('/newsletter', handlers.newsletter)
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
 
 app.use(handlers.notFound)
 app.use(handlers.serverError)
-
-app.get('/newsletter', handlers.newsletter)
-app.post('/api/newsleter-signup', handlers.api.newsletterSignup)
-// @route --- home
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-// @route --- about
-app.get('/about', (req, res) => {
-  res.render('about', { fortune: fortune.getFortune() })
-})
-
-// custom 404 page
-app.use((req, res) => {
-  res.status(404)
-  res.render('404')
-})
-
-// custom 500 page
-app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
-})
 
 if (require.main === module) {
   app.listen(port, () => {
     console.log(
       `Express started on http://localhost:${port}` +
-        ': press Ctrl-C to terminate.'
+        '; press Ctrl-C to terminate.'
     )
   })
 } else {
