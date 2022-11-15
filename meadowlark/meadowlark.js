c07884faf402e0ca9d7527b8df74efb4506ed571
TFuray <pk.master881@gmail.com>
2022-11-14 17:20:22 -0800

    added photo contest views and routes

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -44,19 +44,36 @@ app.get('/newsletter-signup', handlers.newsletterSignup)
 app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
 app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
 
-//handlers for photo submission
-app.post('/contest/vacation-photo/:year/:month', (req, res) => {
-  const form = new multiparty.Form()
-  form.parse(req, (err, fields, files) => {
-    if (err) return res.status(500).send({ error: err.message })
-handlers.vacationPhotoContestProcess(req, res, fields, files)
-  })
-})
-
 // handlers for fetch/JSON form submission
 app.get('/newsletter', handlers.newsletter)
 app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
 
+//handlers for photo submission
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest)
+app.get('/contest/vacation-photo-ajax', handlers.vacationPhotoContestAjax)
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+  const form = new multiparty.Form()
+  form.parse(req, (err, fields, files) => {
+    if (err) return res.status(500).send({ error: err.message })
+    console.log('got fields: ', fields)
+    console.log('and files: ', files)
+    handlers.vacationPhotoContestProcess(req, res, fields, files)
+  })
+})
+app.get(
+  '/contest/vacation-photo-thank-you',
+  handlers.vacationPhotoContestProcessThankYou
+)
+app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
+  const form = new multiparty.Form()
+  form.parse(req, (err, fields, files) => {
+    if (err)
+      return handlers.api.vacationPhotoContestError(req, res, err.message)
+    handlers.api.vacationPhotoContest(req, res, fields, files)
+  })
+})
+
+// handlers for errors
 app.use(handlers.notFound)
 app.use(handlers.serverError)
 
